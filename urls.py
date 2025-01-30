e869ffe90e58384763b52d689695cdc1dcb67d95
Bharti-wagh <bharatiwagh148@gmail.com>
2025-01-30 18:32:01 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,13 @@
+
+# from django.contrib import admin
+# from django.urls import path, include
+# from accounts import views
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('',views.signup_view,name='signup'),
+#     path('dashboard/', views.dashboard,name='dashboard'),
+#     path('accounts/', include('accounts.urls')),
+#     path('leave/', include('leave_management.urls')),
+#     path('payroll/', include('payroll.urls')),
+# ]
+
